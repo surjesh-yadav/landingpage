008c8752cf8da1ec6ff2d2a74f327964004b7084
SurjeshYadavMasai <surjeshom@gmail.com>
2023-12-13 09:36:56 +0530

    commit

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -2,8 +2,15 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div>
-      
+    <div className='container mx-auto p-5 py-16 flex justify-between'>
+         <div className='left-image w-20'>
+            <img src='./navimage.png'/>
+         </div>
+         <div className='right-button bg-[#191919FA] h-10 p-2 rounded-md'>
+           <a href='#' className=''>
+             Connect to Dapp
+           </a>
+         </div>
     </div>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,17 @@ import FasterTransactions from './components/FasterTransactions'
 import BannerSection from "./components/BannerSection"
 import InvestmentOpportunities from "./components/InvestmentOpportunities"
 import RewardsTransaction from "./components/RewardsTransaction"
+import Header from "./components/Header"
+import Footer from "./components/Footer"
 export default function Home() {
   return (
     <div className="bg-[#080312]">
+        <Header/>
         <BannerSection/>
         <InvestmentOpportunities/>
         <FasterTransactions/>
         <RewardsTransaction/>
+        <Footer/>
     </div>
   )
 }
