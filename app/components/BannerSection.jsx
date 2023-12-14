49cda70aafe1e0df56cea1e0d47c3de52f87b241
SurjeshYadavMasai <surjeshom@gmail.com>
2023-12-14 23:10:18 +0530

    done

--- app/components/BannerSection.jsx
+++ app/components/BannerSection.jsx
@@ -7,7 +7,7 @@ const BannerSection = () => {
       <h1 className="heading hidden md:block mb-5 text-[55px] font-bold">
         Decentralized coin with <br /> revenue generation
       </h1>
-      <h1 className="heading block md:hidden mt-28 mb-5 text-[30px] font-bold">
+      <h1 className="heading block md:hidden mt-28 mb-5 text-[32px] font-bold">
         Decentralized coin  with revenue generation
       </h1>
       <p className="description text-[20px] text-left md:text-center line-height-custom mx-auto md:w-1/2">
