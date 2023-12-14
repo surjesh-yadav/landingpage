afd33f7182d783b862f7693727d23040c257c95f
SurjeshYadavMasai <surjeshom@gmail.com>
2023-12-14 21:51:28 +0530

    done

--- app/components/RewardsTransaction.jsx
+++ app/components/RewardsTransaction.jsx
@@ -3,21 +3,25 @@ import React from "react";
 const RewardsTransaction = () => {
   return (
     <div className="text-center my-24">
-      <div className="flex space-x-2 text-white justify-center p-1.5 mx-auto rounded-3xl bg-[#224BC1] md:w-1/4">
+      <div className="flex space-x-2 text-white justify-center max-w-[300px] p-1.5 mx-auto rounded-3xl bg-[#224BC1] md:w-1/4">
         <span>
           <img src="./star.png" />
         </span>
-        <span className="text-[13px] md:text-[14px]">Earn level income upto 10 levels</span>
+        <span className="text-[13px] md:text-[14px]">
+          Earn level income upto 10 levels
+        </span>
       </div>
-      <h2 className="heading text-[33px] font-semibold text-white my-2 text-center">Rewards on every transaction</h2>
+      <h2 className="heading text-[33px] font-semibold text-white my-2 text-center">
+        Rewards on every transaction
+      </h2>
       <p className="md:w-1/3 mt-5 mx-auto text-[15px] text-white">
         Whenever the transaction is made, 15% of the amount is distributed as a
         referral program to all the eligible holders
       </p>
-        <div>
-          <img className="mx-auto hidden md:block my-5" src="/dashboardd.png"/>
-          <img className="mx-auto block md:hidden  my-5" src="/mobile.png"/>
-        </div>
+      <div>
+        <img className="mx-auto hidden md:block my-5" src="/dashboardd.png" />
+        <img className="mx-auto block md:hidden  my-5" src="/mobile.png" />
+      </div>
     </div>
   );
 };
