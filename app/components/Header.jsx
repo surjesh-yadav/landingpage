ae8c061d1820153f69e105a31a2a64794a938a30
SurjeshYadavMasai <surjeshom@gmail.com>
2023-12-15 15:54:13 +0530

    mobile design

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -6,9 +6,9 @@ const Header = () => {
          <div className='left-image'>
             <img src='./logo-dark.svg'/>
          </div>
-         <div className='right-button bg-[#234BC1] h-10 py-2 px-3 md:px-3 rounded-md'>
-           <a href='#' className='flex space-x-1 text-[13px] md:text-[16px] '>
-             <span className='pt-1 md:pt-0'>Connect</span><img src='/header-button-arrow.svg' className="mt-0.5 ml-3" />
+         <div className='right-button bg-[#234BC1] h-10  px-3 md:px-3 rounded-md'>
+           <a href='#' className='flex space-x-1 text-[20px] py-1 md:text-[20px] '>
+             <span className=''>Connect</span><img src='/header-button-arrow.svg' className="mt-0.5 ml-3" />
            </a>
          </div>
     </div>
