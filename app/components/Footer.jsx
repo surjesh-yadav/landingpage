6a959f7e5f530eb87d83b3b8847c63b3dd66fc93
SurjeshYadavMasai <surjeshom@gmail.com>
2023-12-15 15:34:23 +0530

    mobile design

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -8,9 +8,9 @@ const Footer = () => {
         id="scrollToTopBtnmain"
         class="bg-[#224BC1] fixed bottom-20 py-5 hover:bg-blue-700 text-white font-bold px-4 rounded"
       >
-       <img src="./scrollup.png"/>
+       <img src="./scrollup.svg"/>
       </a>
-      <footer class=" md:bg-[#0B0C0E] mt-32">
+      <footer class=" md:bg-[#0B0C0E] pt-32 pb-10">
         <div class="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
           <div class="grid xs:grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5">
             <div className="mb-6 md:mb-0">
